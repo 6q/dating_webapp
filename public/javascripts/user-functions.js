35e2bef2c531ad412efa978f9699b3fc4ec8352c
Xavi Blanch <xavi.blanch@gmail.com>
2013-03-04 10:15:37 +0100

    allow tabs to work when coming from other pages, such as message details

--- public/javascripts/user-functions.js
+++ public/javascripts/user-functions.js
@@ -48,9 +48,18 @@ $(document).ready(function(){
 
   $('#messages-tab a').click(function (e) {
       e.preventDefault();
-        $(this).tab('show');
+      $(this).tab('show');
     });
 
-  //$('#user-block').modal('show');
+  var hash = document.location.hash;
+  var prefix = "tab_";
+  if (hash) {
+      $('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
+  } 
+
+  // Change hash for page-reload
+  $('.nav-tabs a').on('shown', function (e) {
+      window.location.hash = e.target.hash.replace("#", "#" + prefix);
+  });
 
 });
