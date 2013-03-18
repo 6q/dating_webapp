5d739d532ac8a355487a9cf75b311f1bfd9f8c22
Victor Castell <victorcoder@gmail.com>
2013-03-18 21:27:37 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cellove::Application.config.secret_token = '46f976821d54c298a5205cc68ffe2747a0a9047107507792b88b7c857f3d6ff0cc0f7703cad4fa9df0759ba2bfae88c6d5e6c0ecaff0b4d91908d8ef962799a9'
