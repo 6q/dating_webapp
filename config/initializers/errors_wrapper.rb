3fff0beba571dfacd1524877e897497670fb7831
Enric Lluelles <enric@lluell.es>
2013-04-17 17:24:25 +0200

    Output field errors on forms, hidden for now

--- config/initializers/errors_wrapper.rb
+++ config/initializers/errors_wrapper.rb
@@ -1,3 +1,16 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  %{<span class="field_with_errors">#{html_tag}</span>}.html_safe
+  object = instance.object
+  attr_name = instance.method_name
+  errors = instance.object.errors.messages[attr_name.to_sym]
+  out = %{<span class="field_with_errors">#{html_tag}
+    <ul class="error_messages" style="display: none">
+  }
+  errors.each do |e|
+    out += "<li>#{e}</li>"
+  end
+  out += %{
+    </ul>
+    </span>
+  }
+  out.html_safe
 end
