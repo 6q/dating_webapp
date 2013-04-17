af6f760b6ddc9b70292e418026610fb3ba110bb5
Enric Lluelles <enric@lluell.es>
2013-04-17 17:23:38 +0200

    Add roles to users on creation

--- app/controllers/matchmaker_registrations_controller.rb
+++ app/controllers/matchmaker_registrations_controller.rb
@@ -1,2 +1,21 @@
 class MatchmakerRegistrationsController < Devise::RegistrationsController
+  def create
+    @user = user.new(params[:user])
+
+    if @user.save
+      @user.add_role :matchmaker
+      if @user.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(:user, @user)
+        respond_with @user, :location => after_sign_up_path_for(@user)
+      else
+        set_flash_message :notice, :"signed_up_but_#{@user.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with @user, :location => after_inactive_sign_up_path_for(@useer)
+      end
+    else
+      clean_up_passwords @user
+      respond_with @user
+    end
+  end
 end
--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -1,2 +1,21 @@
 class UserRegistrationsController < Devise::RegistrationsController
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      @user.add_role :regular_user
+      if @user.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(:user, @user)
+        respond_with @user, :location => after_sign_up_path_for(@user)
+      else
+        set_flash_message :notice, :"signed_up_but_#{@user.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with @user, :location => after_inactive_sign_up_path_for(@useer)
+      end
+    else
+      clean_up_passwords @user
+      respond_with @user
+    end
+  end
 end
