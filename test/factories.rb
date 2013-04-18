7d2a1bac2cd3e005d6f5f3105315274414b604f8
Enric Lluelles <enric@lluell.es>
2013-04-18 17:59:11 +0200

    Lazy eval of factory attributes

--- test/factories.rb
+++ test/factories.rb
@@ -3,21 +3,21 @@ require 'faker'
 FactoryGirl.define do
   factory :_user do #_because I don't want it to be used directly
     trait :basic_data do
-      name Faker::Name.first_name
-      surname Faker::Name.last_name
-      email Faker::Internet.email
+      name{ Faker::Name.first_name }
+      surname{ Faker::Name.last_name }
+      email{ Faker::Internet.email }
       password "V3RYS3CUR3"
     end
 
     trait :complementary_data do
-      town Faker::Address.city
-      country Faker::Address.country
-      zip_code Faker::Address.zip_code
-      screen_name {[name, surname].join.underscore}
-      marital_status User::MARITAL_STATUS.sample
-      orientation User::ORIENTATION.sample
-      gender User::GENDER.sample
-      birth_date { Date.today << ((rand(10) + 18) * 12) }
+      town{ Faker::Address.city }
+      country{ Faker::Address.country }
+      zip_code{ Faker::Address.zip_code }
+      screen_name{ [name, surname].join.underscore }
+      marital_status{ User::MARITAL_STATUS.sample }
+      orientation{ User::ORIENTATION.sample }
+      gender{ User::GENDER.sample }
+      birth_date{ Date.today << ((rand(10) + 18) * 12) }
       terms_and_conditions "1"
     end
 
