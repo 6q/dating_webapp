ac301b162f78750ab7ee0d082109635c3ec4516c
Enric Lluelles <enric@lluell.es>
2013-04-25 10:50:29 +0200

    Unify profile controllers

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,15 @@
 class ProfilesController < ApplicationController
   layout 'logged_in'
   def show
+    @user = current_user
   end
 
   def update
+  	@user = current_user
+    if @user.update_attributes(params[:user])
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
   end
 end
