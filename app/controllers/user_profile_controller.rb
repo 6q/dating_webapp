4b277135514973f96dc75f0f7756f8e49454b761
Marc Coll <marc@marc-coll.com>
2013-04-25 10:37:41 +0200

    edit profile structure

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -2,13 +2,20 @@ class UserProfileController < ApplicationController
   layout "layouts/logged_in"
 
   def edit
-
-    
-
+    @user = current_user
   end
 
-  def password_change
+  def update
+  	@user = current_user
+    if @user.update_attributes(params[:user])
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
 
+  def update_password
+    redirect_to users_path, :notice => "Ulaa"
   end
 
 end
