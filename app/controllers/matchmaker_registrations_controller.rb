f7add88fe54507a2d04e9a956c723076af0f4e96
Philip De Smedt <philip.desmedt@gmail.com>
2013-05-09 18:16:14 +0200

    Fixed bug where wrong params where used to sign up as a matchmaker (should be params[:matchmaker] instead of params[:user])

--- app/controllers/matchmaker_registrations_controller.rb
+++ app/controllers/matchmaker_registrations_controller.rb
@@ -1,8 +1,8 @@
 class MatchmakerRegistrationsController < Devise::RegistrationsController
   def create
-    @user = User.new(params[:user])
+    @user = User.new(params[:matchmaker])
 
-    if @user.save
+    if @user.save!
       @user.add_role :matchmaker
       if @user.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
