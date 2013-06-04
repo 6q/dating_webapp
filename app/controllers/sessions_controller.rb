7dd88f589602cdbd30311f92660d407ae84f421a
Philip De Smedt <philip.desmedt@gmail.com>
2013-06-04 21:52:15 +0200

    Better error message.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@
 class SessionsController < Devise::SessionsController
 
   def new
-    flash[:error] = _('MESSAGE HERE')
+    flash[:error] = _('Email o contraseño inválidos')
     redirect_to root_path
   end
 
