db1e11107a535d2bb90cbc2a70e58624248e6c8d
Marc Coll <marc@marc-coll.com>
2013-06-07 18:29:42 +0200

    new header markup with placeholder thumb

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,12 +1,15 @@
 #encoding: utf-8
 module UserHelper
-  def profile_pic(user = current_user, options = {width: 210})
+  def profile_pic(user = current_user, options = {width: 210, height: 210})
     pp = user.profile_picture
+    size = "#{options[:width]}x#{options[:height]}#"
     if user.profile_picture
-      size = "#{options[:width]}x#{options[:height]}"
       image_tag(pp.image.thumb(size).url)
     else
-      image_tag("placeholder-user.jpg")
+      app = Dragonfly[:images]
+      uid = app.store(Pathname.new('app/assets/images/placeholder-user.jpg'))
+      image = app.fetch(uid)
+      image_tag(image.thumb(size).url)
     end
   end
 
