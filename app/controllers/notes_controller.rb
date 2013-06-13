5fe4feeddd5a6209f8347b95560e7a8b09c5a7ca
Marc Coll <marc@marc-coll.com>
2013-06-13 17:22:53 +0200

    picture gallery thumb & anti troll tabs/white space

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,25 +1,25 @@
 class NotesController < ApplicationController
-	load_and_authorize_resource
-	before_filter :set_anchor
+  load_and_authorize_resource
+  before_filter :set_anchor
 
-	
-	def create
-		@note = Note.create(params[:note])
+  
+  def create
+    @note = Note.create(params[:note])
     flash[:success] = _('Tu nota se ha guardado.')
-		redirect_to :back
-	end
+    redirect_to :back
+  end
 
-	def destroy
-		@note = Note.find(params[:id])
-		@note.destroy
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
     flash[:success] = _('Tu nota se ha borrado.')
-		redirect_to :back
-	end
+    redirect_to :back
+  end
 
 
-	def set_anchor
-		env["HTTP_REFERER"] += '#tab_notas'
-	end
-	private :set_anchor
+  def set_anchor
+    env["HTTP_REFERER"] += '#tab_notas'
+  end
+  private :set_anchor
 
 end
