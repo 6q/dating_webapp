cded36499d95c1d7e6e1df2128ac57554f737fa3
Victor Castell <victorcoder@gmail.com>
2013-07-17 00:39:19 +0200

    Fix string interpolations fast_gettext

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,4 @@
+require 'gettext_i18n_rails/string_interpolate_fix'
 repos = [
   FastGettext::TranslationRepository.build('pos', path: 'config/locales', type: :po),
   FastGettext::TranslationRepository.build('yamls', path: 'config/locales', type: :yaml)
@@ -6,3 +7,4 @@ FastGettext.add_text_domain('combined', type: :chain, chain: repos)
 FastGettext.default_available_locales = ['es', 'ca'] #all you want to allow
 FastGettext.default_text_domain = 'combined'
 FastGettext.locale = 'es'
+GettextI18nRails.translations_are_html_safe = true
