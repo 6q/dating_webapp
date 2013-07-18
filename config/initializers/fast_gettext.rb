a0dd4370ef9d61a489616fef9bbca69fd8e13eb6
Victor Castell <victorcoder@gmail.com>
2013-07-18 16:54:29 +0200

    Config for locale generation

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,10 +1,6 @@
 require 'gettext_i18n_rails/string_interpolate_fix'
-repos = [
-  FastGettext::TranslationRepository.build('pos', path: 'config/locales', type: :po),
-  FastGettext::TranslationRepository.build('yamls', path: 'config/locales', type: :yaml)
-]
-FastGettext.add_text_domain('combined', type: :chain, chain: repos)
+FastGettext.add_text_domain('app', path: 'config/locales', type: :po)
 FastGettext.default_available_locales = ['es', 'ca'] #all you want to allow
-FastGettext.default_text_domain = 'combined'
+FastGettext.default_text_domain = 'app'
 FastGettext.locale = 'es'
 GettextI18nRails.translations_are_html_safe = true
