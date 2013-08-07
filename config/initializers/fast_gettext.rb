1057d29667ffc8bb8b176995c9458a82a1efa23a
Francesc Gestinet <info@gestinet.com>
2013-08-07 16:29:45 +0200

    Added English language

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,6 +1,6 @@
 require 'gettext_i18n_rails/string_interpolate_fix'
 FastGettext.add_text_domain('app', path: 'config/locales', type: :po)
-FastGettext.default_available_locales = ['es', 'ca'] #all you want to allow
+FastGettext.default_available_locales = ['es', 'ca', 'en'] #all you want to allow
 FastGettext.default_text_domain = 'app'
 FastGettext.locale = 'es'
 GettextI18nRails.translations_are_html_safe = true
