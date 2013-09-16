03304beec499e04d3c48a8f66d2298483bd52fc1
Victor Castell <victorcoder@gmail.com>
2013-09-16 16:57:18 +0200

    Copy message body to activity body in modal

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+
+  $('.message-body').keydown(function() {
+    ab = $(this).closest('.modal').next('.activity-proposal').find('.activity-body')
+    ab.val($(this).val());
+  });
+});
