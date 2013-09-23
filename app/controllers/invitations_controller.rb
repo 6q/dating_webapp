390597c5df44b4e8d7607ce970b0455bd032fbff
Victor Castell <victorcoder@gmail.com>
2013-09-23 18:32:04 +0200

    Send invitation fixes

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,20 +4,20 @@ class InvitationsController < ApplicationController
     invitation = current_user.invitations.build(params[:invitation])
     invitation.invitation_code = SecureRandom.urlsafe_base64
     if invitation.save
-      if params[:invitation][:user_type] == :regular_user
+      if params[:invitation][:user_type] == 'regular_user'
         # Send regular user invitation email
-        UserMailer.invite_user(current_user, params[:invitation][:invited_email], params[:invitation][:invitation_code]).deliver
-        flash[:success] = _('Usuari@ invitado!')
-      elsif params[:invitation][:user_type] == :celestino
+        UserMailer.invite_user(current_user, params[:invitation][:invited_email], invitation.invitation_code).deliver
+        flash[:success] = _('¡Usuari@ invitado!')
+      elsif params[:invitation][:user_type] == 'celestino'
         # Send celestino invitation email
-        UserMailer.invite_celestino(current_user, params[:invitation][:invited_email], params[:invitation][:invitation_code]).deliver
-        flash[:success] = _('Celestin@ invitado!')
+        UserMailer.invite_celestino(current_user, params[:invitation][:invited_email], invitation.invitation_code).deliver
+        flash[:success] = _('¡Celestin@ invitado!')
       end
     else
       # Something went wrong.
       flash[:error] = _('Oops!')
     end
-    redirect_to my_matchmakers_path
+    redirect_to my_matchmakers_path(anchor: 'mejora')
   end
 
 end
