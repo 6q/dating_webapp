df1ee4ba218169b2b33ae133b216534fc0f26dc7
Victor Castell <victorcoder@gmail.com>
2013-09-25 20:49:12 +0200

    Advance in user query

--- app/models/concerns/user_retrieval.rb
+++ app/models/concerns/user_retrieval.rb
@@ -1,68 +1,53 @@
 module UserRetrieval
   extend ActiveSupport::Concern
 
-  SUPER_QUERY = <<-EOF
-  SELECT users.*, COALESCE(v.cnt, 0) as visits_count, COALESCE(l.cnt, 0) as likes_count, COALESCE(r.cnt, 0) as messages_count
-  FROM users
-  LEFT JOIN
-    (SELECT user_id, COUNT(*) AS cnt FROM user_visits WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 50 day) GROUP BY user_id) v
-  ON v.user_id = users.id
-  LEFT JOIN 
-    (SELECT user_id, COUNT(*) AS cnt FROM likes WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 50 day) GROUP BY user_id) l
-  ON l.user_id = users.id
-  LEFT JOIN
-    (SELECT receiver_id, COUNT(*) AS cnt FROM receipts WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 50 day) GROUP BY receiver_id) r
-  ON r.receiver_id = users.id
-  WHERE users.id NOT IN (1)
-  GROUP BY users.id
-  ORDER BY visits_count DESC, likes_count DESC, messages_count DESC, cellove_index DESC
-  LIMIT 5
-EOF
+  def build_query(not_in, order_type = :by_visits, limit = 20)
+    order = {
+      :by_visits => 'visits_count DESC, likes_count DESC, messages_count DESC, cellove_index DESC ',
+      :by_likes => 'likes_count DESC, visits_count DESC, messages_count DESC, cellove_index DESC ',
+    }
+    query = <<-EOF
+      SELECT users.*, COALESCE(v.cnt, 0) as visits_count, COALESCE(l.cnt, 0) as likes_count, COALESCE(r.cnt, 0) as messages_count
+      FROM users
+      LEFT JOIN
+        (SELECT user_id, COUNT(*) AS cnt FROM user_visits WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 50 day) GROUP BY user_id) v
+      ON v.user_id = users.id
+      LEFT JOIN 
+        (SELECT user_id, COUNT(*) AS cnt FROM likes WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 50 day) GROUP BY user_id) l
+      ON l.user_id = users.id
+      LEFT JOIN
+        (SELECT receiver_id, COUNT(*) AS cnt FROM receipts WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 50 day) GROUP BY receiver_id) r
+      ON r.receiver_id = users.id
+    EOF
 
-  def best_rated(radius = User::DEFAULT_NEARBY_DISTANCE)
-    if near = nearbys(radius)
-      hidden_user_ids = self.get_all_invisible_to_me
-      best = near.where("users.id NOT IN (?)", hidden_user_ids)
-        .where(gender: self.matching_gender)
-        .limit(20)
-        .group(:id)
-        .order('cellove_index DESC').sample(5)
-    end
-
-    best
+    query += "WHERE users.id NOT IN (#{not_in.join(',')}) "
+    query += "AND gender = '#{self.matching_gender}' "
+    query += "GROUP BY users.id "
+    query += "ORDER BY #{order[order_type]} "
+    query += "LIMIT #{limit}"
   end
 
   def best_suited_near_me(how_many = 5, radius = User::DEFAULT_NEARBY_DISTANCE)
+    result = User.find_by_sql(build_query(self.get_all_invisible_to_me))
+    @last_query = result.map {|b| b.id}
+    result
+  end
 
-    best = User.find_by_sql(SUPER_QUERY)
-    # max = 0
+  def could_interest_me(how_many = 5, radius = User::DEFAULT_NEARBY_DISTANCE)
+    result = User.find_by_sql(build_query(self.get_all_invisible_to_me + @last_query))
+    @last_query += result.map {|b| b.id}
+    result
+  end
 
-    # begin
-    #   if near = nearbys(radius)
-    #     hidden_user_ids = self.get_all_invisible_to_me
-    #     most_visited = UserVisit.select(:user_id).select('count(user_id)').group(:user_id).map {|u| u.user_id}
-
-    #     most_visited -= hidden_user_ids
-    #     best = near.where(id: most_visited)
-    #     .where(gender: self.matching_gender)
-    #     .limit(how_many).sample(how_many)
-    #   end
-    #   radius += 100
-    #   max += 1
-    # end while(best.count < 5 && max < 5)
-
-    best
+  def best_index(radius = User::DEFAULT_NEARBY_DISTANCE)
+    result = User.find_by_sql(build_query(self.get_all_invisible_to_me + @last_query))
+    @last_query += result.map {|b| b.id}
+    result
   end
 
   def new_users_near_me(radius = User::DEFAULT_NEARBY_DISTANCE)
-    new_near = []
-
-    if near = nearbys(radius)
-      hidden_user_ids = self.get_all_invisible_to_me
-      new_near = near.where("users.id NOT IN (?)", hidden_user_ids)
-        .where(gender: self.matching_gender)
-        .limit(20).sample(5)
-    end
-    new_near
+    result = User.find_by_sql(build_query(self.get_all_invisible_to_me + @last_query))
+    @last_query += result.map {|b| b.id}
+    result
   end
 end
