fbb47645f413d3fa38f5374513cb594e975bac29
Victor Castell <victorcoder@gmail.com>
2013-10-01 01:14:09 +0200

    Nearbys for user rows

--- app/models/concerns/user_retrieval.rb
+++ app/models/concerns/user_retrieval.rb
@@ -40,7 +40,7 @@ module UserRetrieval
     result_ids = result.map {|b| b.id}
     @last_query += result_ids
 
-    result = self.nearbys(100000).where(id: result_ids)
+    result = self.nearbys.where(id: result_ids)
     result
   end
 
