5efc3a0999f44a7db6db89f34ba38a6a97871ea7
6q <francesc.beumala+github@gmail.com>
2013-10-25 18:43:22 +0200

    renamed email subjects and sender email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 class UserMailer < ActionMailer::Base
-  default from: "Cellove <noreply@cellove.com>"
+  default from: "Cellove.com <no-reply@cellove.com>"
 
   def prevent_delivery_to_unconfirmed
     if (@user && !@user.confirmed?) || (@recipient && !@recipient.confirmed?)
@@ -11,21 +11,21 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    mail(:to => user.email, :subject => _('Bienvenido a Cellove'))
+    mail(:to => user.email, :subject => _('Bienvenido a Cellove')._(' | Cellove.com'))
     prevent_delivery_to_unconfirmed
   end
 
   def invite_user(user, email_address, invitation_code)
     @user = user
     @url = new_user_registration_url + '?invitation_code=' + invitation_code
-    mail(:to => email_address, :subject => _('%{name} te invita a Cellove') % {name: user.name})
+    mail(:to => email_address, :subject => _('%{name} te invita a usar Cellove') % {name: user.name})
     prevent_delivery_to_unconfirmed
   end
 
   def invite_celestino(user, email_address, invitation_code)
     @user = user
     @url = new_user_registration_url + '?invitation_code=' + invitation_code
-    mail(:to => email_address, :subject => _('%{name} te invita a Cellove') % {name: user.name})
+    mail(:to => email_address, :subject => _('%{name} te invita a usar Cellove') % {name: user.name})
     prevent_delivery_to_unconfirmed
   end
 
@@ -33,41 +33,41 @@ class UserMailer < ActionMailer::Base
   def receive_message(sender, recipient)
     @sender = sender
     @recipient = recipient
-    mail(:to => @recipient.email, :subject => _('Nuevo mensaje'))
+    mail(:to => @recipient.email, :subject => _('Nuevo mensaje recibido')._(' | Cellove.com'))
     prevent_delivery_to_unconfirmed
   end
 
   def profile_visit(visitor, visited)
     @sender = visitor
     @recipient = visited
-    mail(:to => @recipient.email, :subject => _('Nueva visita de perfil'))
+    mail(:to => @recipient.email, :subject => _('Alguien ha visitado tu perfil')._(' | Cellove.com'))
     prevent_delivery_to_unconfirmed
   end
 
   def like(sender, recipient)
     @sender = sender
     @recipient = recipient
-    mail(:to => @recipient.email, :subject => _('Nuevo me gusta'))
+    mail(:to => @recipient.email, :subject => _('¡Le gustas a alguien!')._(' | Cellove.com'))
     prevent_delivery_to_unconfirmed
   end
 
   def star_rating(sender, recipient)
     @sender = sender
     @recipient = recipient
-    mail(:to => @recipient.email, :subject => _('Nuevo hacemos buena pareja'))
+    mail(:to => @recipient.email, :subject => _('Alguien cree que hace buena pareja contigo')._(' | Cellove.com'))
     prevent_delivery_to_unconfirmed
   end
 
   def celestino_recommendation(sender, recipient)
     @sender = sender
     @recipient = recipient
-    mail(:to => @recipient.email, :subject => _('Nueva recomendación'))
+    mail(:to => @recipient.email, :subject => _('Has recibido una recomendación')._(' | Cellove.com'))
     prevent_delivery_to_unconfirmed
   end
 
   def report(reporter, reported)
     @reporter = reporter
     @reported = reported
-    mail(to: 'cellove@cellove.com', subject: _('Perfil denunciado'))
+    mail(to: 'cellove@cellove.com', subject: _('Perfil denunciado')._(' | Cellove.com'))
   end
 end
