76a8f494baa9cbbce975235fc1c415f00d14ad09
6q <francesc.beumala+github@gmail.com>
2013-11-29 17:38:32 +0100

    removing and trying to do on Nginx configuration

--- config/application.rb
+++ config/application.rb
@@ -78,11 +78,3 @@ module Cellove
     config.assets.version = '1.0'
   end
 end
-module ActionDispatch
-  class Request < Rack::Request
-    alias :remote_ip_orig :remote_ip
-    def remote_ip
-      @remote_ip ||= (@env['HTTP_CF_CONNECTING_IP'] || remote_ip_orig)
-    end
-  end
-end
