c43a7dfc1c5f2ab9fd139d1a24dc29c9412b4431
6q <francesc.beumala+github@gmail.com>
2013-11-29 19:02:31 +0100

    flat pages routes in english and started error page creation

--- config/routes.rb
+++ config/routes.rb
@@ -67,18 +67,19 @@ Cellove::Application.routes.draw do
   get 'background',                to: 'users#get_background',                    as: :get_background
 
   #get 'aviso-legal' => 'flat_pages#legal', as: :legal
-  get 'que-es' => 'flat_pages#what', as: :what
-  get 'como-funciona' => 'flat_pages#how_it_works', as: :how_it_works
-  get 'consigue-popularidad' => 'flat_pages#raise_popularity', as: :raise_popularity
-  get 'celestinos' => 'flat_pages#matchmakers', as: :matchmakers
+  get 'what' => 'flat_pages#what', as: :what
+  get 'how-works' => 'flat_pages#how_it_works', as: :how_it_works
+  get 'raise-popularity' => 'flat_pages#raise_popularity', as: :raise_popularity
+  get 'matchmakers' => 'flat_pages#matchmakers', as: :matchmakers
   get 'terms-and-conditions' => 'flat_pages#terms_conditions', as: :terms_conditions
-  get 'politica-de-privacidad' => 'flat_pages#privacy_policy', as: :privacy_policy
-  get 'consejos-de-uso' => 'flat_pages#usage', as: :usage
+  get 'privacy-policy' => 'flat_pages#privacy_policy', as: :privacy_policy
+  get 'usage' => 'flat_pages#usage', as: :usage
   get 'robots.txt' => 'flat_pages#robots'
-  get 'contacta' => 'flat_pages#new_contact_form', as: :new_contact_form
+  get 'contact-us' => 'flat_pages#new_contact_form', as: :new_contact_form
   post 'contact' => 'flat_pages#contact_form', as: :contact_form
-  get 'media-prensa' => 'flat_pages#media_press', as: :media_press
-  get 'anuncio-tv/:id' => 'flat_pages#tv_spot', as: :tv_spot
+  get 'media-press' => 'flat_pages#media_press', as: :media_press
+  get 'error' => 'flat_pages#error', as: :error
+  get 'tv-spot/:id' => 'flat_pages#tv_spot', as: :tv_spot
 
   resources :messages
   resources :conversations
