0e8e798e7e1e526ba6812fead5bd453f3c923aee
6q <francesc.beumala+github@gmail.com>
2013-12-02 11:07:33 +0100

    trying IP share with CloudFlare

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,4 +40,12 @@ module ApplicationHelper
       I18n.l(from_time.to_date, format: :medium)
     end
   end
+
+  class Request < Rack::Request
+    alias :remote_ip_orig :remote_ip
+    def remote_ip
+      @remote_ip ||= (@env['HTTP_CF_CONNECTING_IP'] || remote_ip_orig)
+    end
+  end
+
 end
