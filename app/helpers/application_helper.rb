41f7d7187210d62ca736599d99f1ec72b5ee85f1
6q <francesc.beumala+github@gmail.com>
2013-12-02 11:26:32 +0100

    removing ip, not working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,12 +42,3 @@ module ApplicationHelper
   end
 
 end
-
-module ActionDispatch
-  class Request < Rack::Request
-    alias :remote_ip_orig :remote_ip
-    def remote_ip
-      @remote_ip ||= (@env['HTTP_CF_CONNECTING_IP'] || remote_ip_orig)
-    end
-  end
-end
