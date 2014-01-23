8837180625b2343cf9e89874b2d0250af4cb9174
David Ramirez <david@davidrv.com>
2014-01-23 20:28:32 +0100

    user registration helper fixed for translations

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,15 +1,15 @@
 #encoding: utf-8
 module RegistrationsHelper
   def gender_options
-    User::GENDER.map {|k,v| [v,k]}
+    {'male' => _('hombre'), 'female' => _('mujer')}.map {|k,v| [v,k]} 
   end
 
   def seeking_options
-    User::SEEKING.map {|k,v| [v,k]}
+    [[_('hombre buscando una mujer'), 'man seeking woman' ], [_('mujer buscando un hombre'), 'woman seeking man' ], [_('hombre buscando un hombre'), 'man seeking man' ], [_('mujer buscando una mujer'), 'woman seeking woman']]
   end
 
   def marital_status_options
-    User::MARITAL_STATUS.map {|k,v| [v,k]}
+    [[_('solter@'),'single'],[_('ocupad@'), 'engaged'], [_('separad@'), 'separated'], [_('divorciad@'), 'divorced'], [_('casad@'), 'married'], [_('viud@'), 'widowed']]
   end
 
   def country_options
@@ -17,148 +17,148 @@ module RegistrationsHelper
   end
 
   def child_options
-    User::CHILD.map {|k,v| [v,k]}
+    [[_('tengo hijos'), 'have-children'], [_('no tengo hijos'), 'no-children'], [_('no tengo hijos, y no quiero tener'), 'no-want-children'], [_('no tengo hijos, pero quiero tener'), 'no-kids-i-want'], [_('si tengo hijos, y no quiero tener mas '), 'have-children-no-want-more'], [_('si tengo hijos, y quiero tener mas '), 'have-children-want-more'], [_('no me gustan los niños'), 'no-like-kids']]
   end
 
   def house_options
-    User::HOUSE.map {|k,v| [v,k]}
+    [[_('sol@'), 'alone'], [_('en pareja'), 'couple'], [_('con mis hijos'), 'with-kids'], [_('ocasionalmente con mis hijos'), 'occasionally-with-children'], [_('acompañad@'), 'accompanied'], [_('con mis padres'), 'with-parents']]
   end
 
   def physical_desc_options
-    User::PHYSICAL_DESC.map {|k,v| [v,k]}
+    [[_('normal'),'normal'], [_('atractiv@'), 'sexy'], [_('guap@'), 'pretty'], [_('del montón'), 'of-the-mill']]
   end
 
   def physical_style_options
-    User::PHYSICAL_STYLE.map {|k,v| [v,k]}
+    [[_('bohemio'), 'bohemian'], [_('clásico'), 'classic'],[_('indie/casual'), 'indie/casual'], [_('deportivo'),'sport'], [_('formal'), 'formal'], [_('a la moda'), 'trendy'], [_('rockero'), 'rocker'], [_('pijo'), 'preppy']]    
   end
 
   def eyes_options
-    User::EYES.map {|k,v| [v,k]}
+    [[_('marrón'), 'brown'],[_('negro'), 'black'], [_('verde'), 'green'],[_('gris'), 'gray'],[_('café'), 'coffee'],[_('azul'), 'blue']]
   end
 
   def hair_options
-    User::HAIR.map {|k,v| [v,k]}
+    [[_('rubio'), 'blond'], [_('pelirrojo'), 'redhead'],[_('gris'), 'gray' ],[_('marrón'), 'brown'],[_('negro'),'black']]
   end
 
   def hair_style_options
-    User::HAIR_STYLE.map {|k,v| [v,k]}
+    [[_('corto'), 'short'],[_('muy corto'),'very short'],[_('largo'), 'large'],[_('afeitado'), 'shaved' ], [_('sin pelo'), 'hairless']]
   end
 
   def complexion_options
-    User::COMPLEXION.map {|k,v| [v,k]}
+    {'thin' => _('delgado'), 'normal' => _('normal'), 'nice' => _('muy bien'), 'athletic' => _('atlético'), 'strong' => _('fuerte'),  'curvy' => _('con curvas'), 'obese' => _('obeso')}.map {|k,v| [v,k]}
   end
 
   def smoke_options
-    User::SMOKE.map {|k,v| [v,k]}
+    {'smoker' => _('Fumo'), 'non-smoker' => _('No fumo'), 'smoker-hater' => _('Soy antitabaco'),'not-mind-smoke' => _('No me molesta el humo'), 'smoke-leave-couple' => _('Fumo pero lo dejaría por mi pareja'), 'social-smoker' => _('Soy fumador social')}.map {|k,v| [v,k]}
   end
 
   def religion_options
-    User::RELIGION.map {|k,v| [v,k]}
+    {'agnostic' => _('agnóstico'), 'atheist' => _('ateo'), 'christian' => _('cristiano'), 'jewish' => _('judío'), 'catholic' => _('católico'), 'muslim' => _('musulmán'), 'hindu' => _('indú'), 'buddhist' => _('budista')}.map {|k,v| [v,k]}
   end
 
   def ethnicity_options
-    User::ETHNICITY.map {|k,v| [v,k]}
+    {'hispanic' => _('hispana'), 'arab' => _('árabe'), 'indian' => _('india'), 'european' => _('europea'), 'african' => _('africana'), 'asian' => _('asiática') }.map {|k,v| [v,k]}
   end
 
   def language_options
-    User::LANGUAGE.map {|k,v| [v,k]} 
+    { 'english' => _('Inglés'), 'spanish' => _('Español'), 'french' => _('Francés'), 'italian' => _('Italiano'), 'other' => _('otro') }.map {|k,v| [v,k]} 
   end
 
   def language_level_options
-    User::LANGUAGE_LEVEL.map {|k,v| [v,k]} 
+    { 'low' => _('bajo'), 'medium' => _('medio'), 'hight' => _('alto') }.map {|k,v| [v,k]} 
   end
 
   def study_level_options
-    User::STUDY_LEVEL.map {|k,v| [v,k]}
+    {'school' => _('instituto o inferior'), 'high-school' => _('bachillerato'),'certified' => _('diplomado'), 'professional' => _('módulo profesional'), 'graduate' => _('licenciado o superior')}.map {|k,v| [v,k]}
   end
 
   def job_options
-    User::JOB.map {|k,v| [v,k]}
+    {'artistic' => _('trabajos artísticos y creativos '),'banking' => _('banca, financiero'),'administrative' => _('adminsitrativo'), 'technology' => _('ordenadores y tecnología'),'construction' => _('construcción'),'education' => _('educación'), 'advertising' => _('publicidad, entretenimiento y medios'),'management' => _('ejecutivo y gestión '),'health'=> _('salud'), 'travel' => _('viajes'),'legal' => _('servicios legales'),'military' => _('militar'),'employee' => _('empleado'), 'public-employee' => _('empleado público / gobierno'),'science' => _('ciencia'), 'sales' => _('ventas y márketing'), 'transportation' => _('transporte y logística '),'unemployed' => _('desempleado'),'retired' => _('jubilado'), 'retired-temporarily' => _('retirado temporalmente'),'studying' => _('estoy estudiando')}.map {|k,v| [v,k]}
   end
 
   def diet_options
-    User::DIET.map {|k,v| [v,k]}
+    {'vegetarian' => _('vegetariana'), 'vegan' => _('vegana'), 'japanese' => _('japonesa'), 'mediterranean' => _('mediterránea'), 'italian' => _('italiana'), 'kosher' => _('kosher'), 'halal' => _('halal'), 'obsessive-food' => _('soy muy maniátic@ con la comida'), 'like-all' => _('me gusta todo'), 'adapt' => _('me adapto fácilmente')}.map {|k,v| [v,k]}
   end
 
   def alcohol_options
-    User::ALCOHOL.map {|k,v| [v,k]}
+    {'when-go-out' => _('me gusta beber solo cuando salgo'), 'not-drink' => _('no bebo alcohol'), 'occasionally' => _('bebo ocasionalmente'), 'drinker' => _('soy bebedor'), 'meke-me-sick' => _('el alcohol no me sienta bien')}.map {|k,v| [v,k]}
   end
 
   def wedding_opinion_options
-    User::WEDDING_OPINION.map {|k,v| [v,k]}
+    {'would-marry' => _('Me gustaría casarme'), 'would-not-marry' => _('No me gustaría casarme'), 'not-believe' => _('No creo en el matrimonio'), 'married-no-repeat'=> _('Ya he estado casado y no quiero repetir'), 'married-repeat' => _('Ya he estado casado y quiero repetir'), 'wonderful' => _('El matrimonio es algo maravilloso')}.map {|k,v| [v,k]}
   end
 
   def religion_activity_options
-    User::RELIGION_ACTIVITY.map {|k,v| [v,k]}
+    {'essential' => _('para mi la religión es esencial en mi vida'), 'care-little' => _('me importa poco la religión'), 'not-care' => _('no me importa en absoluto la religión')}.map {|k,v| [v,k]}
   end
 
   def animals_options
-    User::ANIMALS.map {|k,v| [v,k]}
+    {'like-pets' => _('Me gustan los animales de compañía'), 'not-like-pets' => _('No me gustan los animales de compañía'), 'have-pets' => _('Tengo animales de compañía'), 'no-pets' => _('No tengo animales de compañía'), 'allergic' => _('No tengo animales de compañía y soy alérgico')}.map {|k,v| [v,k]}
   end
 
   def party_options
-    User::PARTY.map {|k,v| [v,k]}
+    {'like-out' => _('Me gusta mucho salir'), 'not-like-out' => _('No me gusta salir'), 'like-bars' => _('Me gusta ir de bares'), 'like-disco' => _('Me gusta ir a la discoteca'), 'like-drink-friends' => _('Me gusta ir a tomar algo con amigos'), 'love-party' => _('Me encanta la fiesta'), 'not-like-party' => _('No me gusta nada la fiesta')}.map {|k,v| [v,k]}
   end
 
   def music_options
-    User::MUSIC.map {|k,v| [v,k]}
+    {'like-music' => _('Me gusta la música'), 'no-like-music' => _('no me gusta la música')}.map {|k,v| [v,k]}
   end
 
   def music_genre_options
-    User::MUSIC_GENRE.map {|k,v| [v,k]}
+    {'pop' => _('pop'), 'rock' => _('rock'), 'classical' => _('clásica'), 'opera' => _('ópera'), 'heavy' => _('heavy'), 'everything' => _('de todo un poco')}.map {|k,v| [v,k]}
   end
 
   def cinema_options
-    User::CINEMA.map {|k,v| [v,k]}
+    {'like-cinema' => _('Me gusta ir al cine'), 'not-like-cinema' => _('No me gusta ir al cine')}.map {|k,v| [v,k]}
   end
 
   def cinema_frequency_options
-    User::CINEMA_FREQUENCY.map {|k,v| [v,k]}
+    {'occasionally' => _('ocasionalemnete'), 'often' => _('muy a menudo')}.map {|k,v| [v,k]}
   end
 
   def cinema_genre_options
-    User::CINEMA_GENRE.map {|k,v| [v,k]}
+    {'romantic' => _('romántico'), 'dramas' => _('drama'), 'comedies' => _('comedia'), 'police' => _('policíaco'), 'terror' => _('terror')}.map {|k,v| [v,k]}
   end
 
 
   def lf_relationship_options
-    User::LF_RELATIONSHIP.map {|k,v| [v,k]}
+    {'friends' => _('hacer nuevas amistades'), 'fun' => _('salir y pasarlo bien'), 'couple' => _('establecer una relación estable'), 'marry' => _('casarme')}.map {|k,v| [v,k]}
   end
 
   def lf_house_options
-    User::LF_HOUSE.map {|k,v| [v,k]}
+    {'alone' => _('sol@'),'couple' => _('en pareja'),'with-kids' => _('con sus hijos'), 'occasionally-with-children' => _('ocasionalmente con sus hijos'),'accompanied' => _('acompañad@')}.map {|k,v| [v,k]}
   end
 
   def lf_child_options
-    User::LF_CHILD.map {|k,v| [v,k]}
+    { 'have-children' => _('tenga hijos'),'no-children' => _('no tenga hijos'),'no-want-children' => _('no tenga hijos, y no quiera tener'), 'no-kids-i-want' => _('no tenga hijos, pero quiera tener'),'have-children-no-want-more' => _('tenga hijos, y no quiera tener mas '), 'have-children-want-more' => _('tenga hijos, y quiera tener mas '),'no-like-kids' => _('no le gusten los niños')}.map {|k,v| [v,k]}
   end
 
   def lf_smoke_options
-    User::LF_SMOKE.map {|k,v| [v,k]}
+    {'smoker' => _('fume'), 'non-smoker' => _('no fume'), 'smoker-hater' => _('sea antitabaco'), 'not-mind-smoke' => _('no le moleste el humo'), 'smoke-leave-couple' => _('fume pero lo dejaría por su pareja'), 'social-smoker' => _('sea fumador social')}.map {|k,v| [v,k]}
   end
 
   def lf_party_options
-    User::LF_PARTY.map {|k,v| [v,k]}
+    {'like-out' => _('le guste mucho salir'), 'not-like-out' => _('no le guste salir'), 'like-bars' => _('le guste ir de bares'), 'like-disco' => _('le guste ir a la discoteca'), 'like-drink-friends' => _('le guste ir a tomar algo con amigos'), 'love-party' => _('le encante la fiesta'), 'not-like-party' => _('no le guste nada la fiesta')}.map {|k,v| [v,k]}
   end
 
   def lf_diet_options
-    User::LF_DIET.map {|k,v| [v,k]}
+    {'vegetarian' => _('vegetariana'), 'vegan' => _('vegana'), 'japanese' => _('japonesa'), 'mediterranean' => _('mediterránea'), 'italian' => _('italiana'), 'kosher' => _('kosher'), 'halal' => _('halal'), 'obsessive-food' => _('sea muy maniátic@ con la comida'), 'like-all' => _('le guste todo'), 'adapt' => _('se adapte fácilmente')}.map {|k,v| [v,k]}
   end
 
   def lf_alcohol_options
-    User::LF_ALCOHOL.map {|k,v| [v,k]}
+    {'when-go-out' => _('le guste beber solo cuando salga'), 'not-drink' => _('no beba alcohol'), 'occasionally' => _('beba ocasionalmente'), 'drinker' => _('sea bebedor'), 'meke-me-sick' => _('el alcohol no le sienta bien')}.map {|k,v| [v,k]}
   end
 
   def lf_religion_activity_options
-    User::LF_RELIGION_ACTIVITY.map {|k,v| [v,k]}
+    {'essential' => _('la religión sea esencial en su vida'), 'care-little' => _('le importe poco la religión'), 'not-care' => _('no le importe en absoluto la religión')}.map {|k,v| [v,k]}
   end
 
   def lf_animals_options
-    User::LF_ANIMALS.map {|k,v| [v,k]}
+    {'like-pets' => _('le gusten los animales de compañía'), 'not-like-pets' => _('no le gusten los animales de compañía'), 'have-pets' => _('tenga animales de compañía'), 'no-pets' => _('no tenga animales de compañía'), 'allergic' => _('no tenga animales de compañía y sea alérgic@')}.map {|k,v| [v,k]}
   end
 
   def lf_child_options
-    User::LF_CHILD.map {|k,v| [v,k]}
+    { 'have-children' => _('tenga hijos'),'no-children' => _('no tenga hijos'),'no-want-children' => _('no tenga hijos, y no quiera tener'), 'no-kids-i-want' => _('no tenga hijos, pero quiera tener'),'have-children-no-want-more' => _('tenga hijos, y no quiera tener mas '), 'have-children-want-more' => _('tenga hijos, y quiera tener mas '),'no-like-kids' => _('no le gusten los niños')}.map {|k,v| [v,k]}
   end
 
 
