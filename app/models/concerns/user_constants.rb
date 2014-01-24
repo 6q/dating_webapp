611bf59e8fbf55ac46d0d07e2bba5c65a7595691
David Ramirez <david@davidrv.com>
2014-01-24 19:03:23 +0100

    more _gustos fixes

--- app/models/concerns/user_constants.rb
+++ app/models/concerns/user_constants.rb
@@ -47,7 +47,7 @@ module UserConstants
   # THIRD PERSON OPTIONS
   TP_CHILD = { 'have-children' => _('tiene hijos'),'no-children' => _('no tiene hijos'),'no-want-children' => _('no tiene hijos, y no quiere tener'), 'no-kids-i-want' => _('no tiene hijos, pero quiere tener'),'have-children-no-want-more' => _('tiene hijos, y no quiere tener mas '), 'have-children-want-more' => _('si tiene hijos, y quiere tener mas '),'no-like-kids' => _('no le gustan los niños')}
   TP_HOUSE = {'alone' => _('sol@'),'couple' => _('en pareja'),'with-kids' => _('con sus hijos'), 'occasionally-with-children' => _('ocasionalmente con sus hijos'),'accompanied' => _('acompañad@'), 'with-parents' => _('con sus padres')}
-  TP_SMOKE = {'smoker' => 'fuma', 'non-smoker' => 'no fuma', 'smoker-hater' => 'es antitabaco','not-mind-smoke' => _('no le molesta el humo'), 'smoke-leave-couple' => _('fuma pero lo dejaría por su pareja'), 'social-smoker' => _('es fumador social')}
+  TP_SMOKE = {'smoker' => 'fuma', 'non-smoker' => 'no fuma', 'smoker-hater' => 'es antitabaco', 'not-mind-smoke' => _('no le molesta el humo'), 'smoke-leave-couple' => _('fuma pero lo dejaría por su pareja'), 'social-smoker' => _('es fumador social')}
   TP_DIET = {'vegetarian' => _('vegetariana'), 'vegan' => _('vegana'), 'japanese' => _('japonesa'), 'mediterranean' => _('mediterránea'), 'italian' => _('italiana'), 'kosher' => _('kosher'), 'halal' => _('halal'), 'obsessive-food' => _('es muy maniátic@ con la comida'),
     'like-all' => _('le gusta todo'), 'adapt' => _('se adapta fácilmente')}
   TP_ALCOHOL = {'when-go-out' => _('le gusta beber solo cuando sale'), 'not-drink' => _('no bebe alcohol'), 'occasionally' => _('bebe ocasionalmente'), 'drinker' => _('es bebedor'), 'meke-me-sick' => _('el alcohol no le sienta bien')}
