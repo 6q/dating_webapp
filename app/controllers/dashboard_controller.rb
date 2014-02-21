6ea67f6e7617d69f3c809ed16319ad651804b159
Francesc Leveque <francesc@leveque.es>
2014-02-21 09:13:15 +0100

    Removed commented code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,21 +9,7 @@ class DashboardController < ApplicationController
   # before_filter :is_invited_user?
 
   def show
-
     @best_suited_near_me = current_user.best_suited_near_me
-
-    #could_interest_me = current_user.could_interest_me.sample(20)
-    #@could_interest_me = Kaminari.paginate_array(could_interest_me).page(params[:page]).per(5)
-    #session[:could_interest_me] = could_interest_me.map(&:id)
-
-    # best_index = current_user.best_index.sample(20)
-    # @best_index = Kaminari.paginate_array(best_index).page(params[:page]).per(5)
-    # session[:best_index] = best_index.map(&:id)
-
-    # new_users_near_me = current_user.new_users_near_me.sample(20)
-    # @new_users_near_me = Kaminari.paginate_array(new_users_near_me).page(params[:page]).per(5)
-    # session[:new_users_near_me] = new_users_near_me.map(&:id)
-
     @search = User.search
   end
 
