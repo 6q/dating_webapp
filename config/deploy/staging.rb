60f589535ac69b715bc727064f2b20e82de239b6
Francesc Leveque <francesc@leveque.es>
2014-02-28 10:37:24 +0100

    staging server config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,11 @@
-set :user, "cellove"
-set :domain, "cellove.quantic.es"
+set :user, "deploy"
+set :domain, "staging.cellove.com"
 set(:branch) { ENV.fetch('BRANCH', 'develop') }
 
 role :app, domain
 role :web, domain
 role :db, domain, :primary => true
 
-ssh_options[:port] = 2273
+ssh_options[:port] = 22
 
 require 'capistrano-unicorn'
