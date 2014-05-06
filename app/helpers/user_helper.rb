131ce8353e78388c96dd9aa0336c33180d7e9c1e
Francesc Leveque <francesc@leveque.es>
2014-05-06 10:38:53 +0200

    bugfix

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,15 +1,16 @@
 #encoding: utf-8
 module UserHelper
   def profile_pic_url(user, options)
-    size = "#{options[:width]}x#{options[:height]}#"
-    url = nil
+    size  = "#{options[:width]}x#{options[:height]}#"
+    url   = nil
     if user && profile_pic = user.profile_picture
       url = profile_pic.image.thumb(size).url
     else
-      app = Dragonfly[:images]
-      uid = app.store(Pathname.new(Rails.application.config.assets.paths.first + "/placeholder-#{user.gender || 'male'}-#{Random.rand(1..3)}.jpg"))
-      image = app.fetch(uid)
-      url = image.thumb(size).url
+      app         = Dragonfly[:images]
+      user_gender = user.nil? ? 'male' : (user.gender || 'male')
+      uid         = app.store(Pathname.new(Rails.application.config.assets.paths.first + "/placeholder-#{user_gender}-#{Random.rand(1..3)}.jpg"))
+      image       = app.fetch(uid)
+      url         = image.thumb(size).url
     end
 
     url
@@ -20,7 +21,7 @@ module UserHelper
   end
 
   def user_action_links
-    html = '<li class="message">' + link_to(_('Enviar Mensaje'), '#fast-reply', 'data-toggle' => 'modal', title: _('Enviar Mensaje')) + '</li>'
+    html  = '<li class="message">' + link_to(_('Enviar Mensaje'), '#fast-reply', 'data-toggle' => 'modal', title: _('Enviar Mensaje')) + '</li>'
     html += '<li class="chat">' + link_to('Chat', '/user/chat/', title: 'Chat') + '</li>'
     html += '<li class="like">' + link_to(_('Me gusta'), '/user/likes-of-mine/', title: _('Me gusta')) + '</li>'
     html += '<li class="block">' + link_to(_('Bloquear'), '#user-block', 'data-toggle' => 'modal', title: _('Bloquear')) + '</li>'
@@ -35,7 +36,7 @@ module UserHelper
     if !user.has_role?(:regular_user)
       return ""
     end
-    html = '<div class="user-detail clearfix">'
+    html  = '<div class="user-detail clearfix">'
     html += user_link_with_picture(user)
     html += "<div class=\"data\">#{link_to(user.name, user)}, #{user.age} Años - #{user.city}</div>"
     html += '</div>'
