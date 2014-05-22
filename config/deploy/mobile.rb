fdebb410c617e548bd44d7d219b9cc2416d9ccb5
Francesc Leveque <francesc@leveque.es>
2014-05-22 11:48:12 +0200

    Trying to create a new deploy environment for mobile branch

---
+++ config/deploy/mobile.rb
@@ -0,0 +1,15 @@
+set :user, "deploy"
+set :domain, "mobile.cellove.com"
+set(:branch) { ENV.fetch('BRANCH', 'mobile') }
+
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+
+ssh_options[:port] = 22
+
+require 'capistrano-unicorn'
+
+set :whenever_environment, defer { stage }
+set :whenever_identifier, defer { "#{application}_#{stage}" }
+require "whenever/capistrano"
