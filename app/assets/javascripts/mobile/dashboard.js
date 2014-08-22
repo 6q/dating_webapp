56d61eec12a4279049cab0f5db7410c1668f835d
6q <francesc.beumala+github@gmail.com>
2014-08-22 11:26:45 +0200

    mobile dasboard fist aproach

---
+++ app/assets/javascripts/mobile/dashboard.js
@@ -0,0 +1,17 @@
+$(function() {
+  $(document).on('click', '.paginate-users', function(e) {
+    e.preventDefault();
+
+    section = $(this).closest('section');
+    section.find('ul').replaceWith('<div style="height: 264px"><img src="/assets/ajax-loader.gif" style="margin-top: 65px; margin-left: 365px">');
+    $.ajax({
+      url: this.href,
+      success: function(data) {
+        section = $('#' + data.id)
+        section.replaceWith(data.template);
+        likes_bind();
+        $( "[data-toggle='tooltip']" ).tooltip({html:true});
+      }
+    });
+  })
+});
