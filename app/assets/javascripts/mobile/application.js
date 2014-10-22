a2dc88cfd23914f65d83d1df07b5d83823ef2f4b
6q <francesc.beumala+github@gmail.com>
2014-10-22 18:56:38 +0200

    ios app links

--- app/assets/javascripts/mobile/application.js
+++ app/assets/javascripts/mobile/application.js
@@ -134,7 +134,7 @@ $(document).ready(function(){
     if (("standalone" in window.navigator) && window.navigator.standalone) {
       // For iOS Apps
       $('a').on('click', function(e){
-        e.preventDefault();
+        if ($( e.target ).prop( "tagName" ) !== "IMG") e.preventDefault();
         var new_location = $(this).attr('href');
         if (new_location != undefined && new_location.substr(0, 1) != '#' && $(this).attr('data-method') == undefined){
           window.location = new_location;
